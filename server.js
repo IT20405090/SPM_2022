aaa8951535233532f8c48880d12c1588ceff95a1
Ramona Vanhoff <87459049+IT20405090@users.noreply.github.com>
2022-09-02 12:19:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+//import Routes 
+const user = require("./Routes/userRoutes");
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "*", 
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+app.use("/user",user);
+
+
+
+mongoose.connect(
+  process.env.DB_URL, {
+    //type warnings
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  .then(() => {
+    console.log("Mongo DB connected successfully");
+  })
+
+  .catch((err) => console.log("DB connection failed", err));
+
+app.listen(PORT, () => {
+  console.log(`Backend App is running on ${PORT}`);
+});
+
+
+
